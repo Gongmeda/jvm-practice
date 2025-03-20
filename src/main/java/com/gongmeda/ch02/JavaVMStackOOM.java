2c93b754ff6fa8a30cb90c94dfd446318741914a
Gongmeda <gongmeda@gmail.com>
2025-03-20 22:55:18 +0900

    ch02

---
+++ src/main/java/com/gongmeda/ch02/JavaVMStackOOM.java
@@ -0,0 +1,20 @@
+package com.gongmeda.ch02;
+
+public class JavaVMStackOOM {
+    private void dontStop() {
+        while (true) {
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(this::dontStop);
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
